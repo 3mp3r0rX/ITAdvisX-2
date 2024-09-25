1c89aaaaf74c0e2a3937683fffd9ab3d94f4e8ee
mremperorx <majedalnasr7@gmail.com>
2024-09-25 13:28:05 +0300

    AOS added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useState, useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import { Banner } from "@/components/Banner";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
@@ -11,7 +13,7 @@ import { FAQs } from "@/components/FAQs";
 import { CTA } from "@/components/CTA";
 import { Footer } from "@/components/Footer";
 import FadeInTextAnimation from "@/components/FadeInTextAnimation";
-import LoadingSpinner from "@/components/LoadingSpinner"; // Import your Loading Spinner component
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -19,24 +21,40 @@ export default function Home() {
   useEffect(() => {
     setTimeout(() => setLoading(false), 2000); 
 
+    AOS.init({
+      duration: 1200, // Animation duration in milliseconds
+      once: true,     // Only animate once when scrolling down
+    });
+
     return () => setLoading(true);
   }, []);
 
   return (
     <>
       {loading ? (
-        <LoadingSpinner /> 
+        <LoadingSpinner />
       ) : (
         <FadeInTextAnimation>
-          <Banner />
-          <Navbar />
-          <Hero />
-          <LogoTicker />
-          <Features />
-          <ProductShowcase />
-          <FAQs />
-          <CTA />
-          <Footer />
+            <Banner />
+            <Navbar />
+          <div data-aos="fade-right">
+            <Hero />
+          </div>
+          <div data-aos="flip-left">
+            <Features />
+          </div>
+          <div data-aos="flip-up">
+            <ProductShowcase />
+          </div>
+          <div data-aos="fade-up">
+            <FAQs />
+          </div>
+          <div data-aos="fade-up">
+            <CTA />
+          </div>
+          <div data-aos="fade-up">
+            <Footer />
+          </div>
         </FadeInTextAnimation>
       )}
     </>
